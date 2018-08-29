21abd6996eecb3315cc2424f83b717d90a36a0a1
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-29 15:49:10 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    set :session_secret, "secret"
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout' do
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
 end
